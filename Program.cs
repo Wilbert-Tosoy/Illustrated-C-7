e9d42b376d79b39b929368b82d921608fa9e5652
Wilbert-Tosoy <brownabelard@gmail.com>
2022-09-13 20:44:10 +0800

    Add a.

--- Program.cs
+++ Program.cs
@@ -6261,7 +6261,7 @@ namespace Illustrated_CSharp_7
             Clear();
 
             WriteLine("\nInvoking Delegate with Return Values\n" +
-                "\t- If ");
+                "\t- If a");
             ReadKey();
 
             Clear();
